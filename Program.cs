feb096b78fad5e9efce5ce425c49452b6d53f389
Batuhan Topcu <topcubatuhan00@gmail.com>
2026-01-11 16:43:56 +0300

    inital start app

---
+++ Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/getall", () => Results.Ok(new List<string>()
+{
+    "EXAMPLE 1",
+    "EXAMPLE 2"
+}));
+
+app.Run();
